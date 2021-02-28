bd20829726ca994d71094bedd15ae0e83be4ad70
dauz20 <72052839+dauz20@users.noreply.github.com>
2021-02-28 09:10:30 +0700

    Add files via upload

---
+++ 10. StudyCase1.js
@@ -0,0 +1,15 @@
+//Study Case I
+const umur = 3;
+if(umur >= 0 && umur <= 1) {
+  console.log("Bayi");
+} else if(umur >= 2 && umur <= 10) {
+  console.log("Anak-anak");
+} else if(umur >= 11 && umur <= 19) {
+  console("Remaja");
+} else if(umur >= 20 && umur <= 60) {
+  console.log("Dewasa");
+} else {
+  console.log("E");
+}
+
+//Umur 3 tahun adalah anak-anak
